c25c8f4842e3eceec3951e13ade43b3757eff49b
Kaleb Brunner <127056216+KalebBrunner@users.noreply.github.com>
2025-11-28 13:30:51 -0500

    Let Electron use vue components

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// import FileExplorer from "./pages/FileExplorer.vue";
+// import Welcome from "./pages/Welcome.vue";
+</script>
+
+<template>
+
+    Test Hello from app.vue
+
+  <!-- <Welcome /> -->
+  <!-- <FileExplorer /> -->
+</template>
--- src/renderer.ts
+++ src/renderer.ts
@@ -27,5 +27,8 @@
  */
 
 import './index.css';
+import { createApp } from "vue";
+import App from "./App.vue";
 
+createApp(App).mount("#app");
 console.log('👋 This message is being logged by "renderer.ts", included via Vite');
